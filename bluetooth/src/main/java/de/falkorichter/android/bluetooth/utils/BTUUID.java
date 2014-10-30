667e93adb3b0e43cf3897b1a8dc62ab3dfd1697b
Falko Richter <falko@briefhansa.de>
2014-10-30 18:04:27 +0100

    correct order

--- bluetooth/src/main/java/de/falkorichter/android/bluetooth/utils/BTUUID.java
+++ bluetooth/src/main/java/de/falkorichter/android/bluetooth/utils/BTUUID.java
@@ -5,12 +5,12 @@ import java.util.UUID;
 public class BTUUID {
 
     public static class Characteristic{
-        public static final UUID manufacturer_name_string   = BTUUID.fromString("2A29");
         public static final UUID model_number_string        = BTUUID.fromString("2A24");
         public static final UUID serial_number_string       = BTUUID.fromString("2A25");
         public static final UUID firmware_revision_string   = BTUUID.fromString("2A26");
         public static final UUID hardware_revision_string   = BTUUID.fromString("2A27");
         public static final UUID software_revision_string   = BTUUID.fromString("2A28");
+        public static final UUID manufacturer_name_string   = BTUUID.fromString("2A29");
         public static final UUID ieee1107320601_regulatory_certification_data_list = BTUUID.fromString("2A2A");
     }
 
